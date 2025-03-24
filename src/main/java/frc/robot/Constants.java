505efc2e4142752a05ae96658f7b5c03541c8f8a
kmbrinkmann <kmbrinkm@outlook.com>
2025-03-24 17:45:21 -0500

    clean up

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -222,8 +222,7 @@ public class Constants {
 
     public static final class SwerveConstants {
         public static final double percentSlow = 0.35;
-        public static final double RAMP_STEP = 0.1;
-        public static final double smoothingFactor = 0.65;
+        public static final double smoothingFactor = 0.5;
         // Closer to 1: Faster changes (less smoothing), Closer to 0: Slower changes (more smoothing).
     } 
 
