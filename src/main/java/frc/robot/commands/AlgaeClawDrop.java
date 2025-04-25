b3882679c852cc3e68dacaf1e1a4f9c99c7353e8
Miles Helvey <151591635+mhelve46@users.noreply.github.com>
2025-04-25 09:48:51 -0500

    Update AlgaeClawDrop.java

--- src/main/java/frc/robot/commands/AlgaeClawDrop.java
+++ src/main/java/frc/robot/commands/AlgaeClawDrop.java
@@ -1,20 +1,5 @@
 package frc.robot.commands;
 
 public class AlgaeClawDrop extends Command {
-
   public AlgaeClawDrop() {}
-
-  @Override
-  public void initialize() {}
-
-  @Override
-  public void execute() {}
-
-  @Override
-  public void end(boolean interrupted) {}
-
-  @Override
-  public boolean isFinished() {
-    return true;
-    }
 }
